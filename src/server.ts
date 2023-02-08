899cf1d726e4b6d14bb9f54724e3b72e98d99261
jake-the-cake <askjake331@gmail.com>
2023-02-08 14:36:30 -0500

    building out router

--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,31 @@ const port = 4200
 const host = 'localhost'
 
 const server = http.createServer(function(req,res){
-  console.log(req)
+  const reqUrl = isRoute(req.url!)
+  if (reqUrl) {
+    console.log('is a route')
+  }
+  console.log(req.url)
+
   res.end()
 })
 
 server.listen(port, host, undefined, () => { console.log('listening?')})
+
+function isRoute(url: string): string | null {
+  let isValid: boolean = false
+  const splitUrl = url.split('/')
+  if (splitUrl.length > 1) {
+    console.log('valid route')
+    const splitEnd = splitUrl[splitUrl.length - 1].split('.')
+    // if (splitEnd[splitEnd.length - 1] !== '')
+  }
+  else return null
+
+  switch (isValid) {
+    case false:
+      return null
+    default:
+      return url
+  }
+}
