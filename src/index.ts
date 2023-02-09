ce246af348653907a5a3d5c92aa28156d969a216
jake-the-cake <askjake331@gmail.com>
2023-02-09 15:25:37 -0500

    server class

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
-import { goatServer, goatUp } from './server'
+import { goatServer, GoatTail, goatUp } from './server'
 
 const port = 4200
 const host = 'localhost'
 
-const server = goatServer()
+// const server = goatServer()
+const {server} = new GoatTail().port(1234).message(`Server running on port ${port}`).run()
+// console.log(server)
 
-goatUp(server, port, host, `Server running on port ${port}`)
+// if (server) goatUp(server, port, host, `Server running on port ${port}`)
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,44 @@
 import http from 'http'
 import { isRoute } from './middleware/isRoute'
 
+export class GoatTail {
+  portNumber: number
+  host: string
+  messageText?: string
+  server?: http.Server<typeof http.IncomingMessage, typeof http.ServerResponse>
+  constructor(){
+    this.portNumber = 6047
+    this.host = 'localhost'
+    this.newServer()
+    return this
+  }
+  newServer(){
+    this.server = http.createServer(function(req,res){
+      const reqUrl = isRoute(req.url!)
+      if (reqUrl) {
+        console.log('is a route')
+        console.log(reqUrl)
+      }
+      res.end()
+    })
+    return this
+  }
+  port(port: number){
+    if (port) this.portNumber = port
+    return this
+  }
+  message(message: string){
+    if (message) this.messageText = message
+    return this
+  }
+  run(){
+    const message = this.messageText || ''
+    this.server?.listen(this.portNumber, this.host, undefined, function(){ console.log(message) })
+    return this
+  }
+}
+
+
 export function goatServer(){
   return http.createServer(function(req,res){
     const reqUrl = isRoute(req.url!)
