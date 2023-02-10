abf113749b6eeddd4f248b537ef50247e5ff4d3a
Jakey Wakey <askjake331@gmail.com>
2023-02-09 20:07:30 -0500

    launcher built

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,8 @@
-import { goatServer, GoatTail, goatUp } from './server'
+import { GoatTail } from './server'
 
 const port = 4200
 const host = 'localhost'
 
-// const server = goatServer()
-const {server} = new GoatTail().port(1234).message(`Server running on port ${port}`).run()
-// console.log(server)
-
-// if (server) goatUp(server, port, host, `Server running on port ${port}`)
+new GoatTail('fake')
+	.message(`Angela is the best!`)
+	.run()
--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,13 @@ import http from 'http'
 import { isRoute } from './middleware/isRoute'
 
 export class GoatTail {
+  name?: string
   portNumber: number
   host: string
   messageText?: string
   server?: http.Server<typeof http.IncomingMessage, typeof http.ServerResponse>
-  constructor(){
+  constructor(name: string | undefined = undefined){
+    if (name) this.name = name[0].toUpperCase() + name.slice(1).toLowerCase()
     this.portNumber = 6047
     this.host = 'localhost'
     this.newServer()
@@ -32,23 +34,23 @@ export class GoatTail {
     return this
   }
   run(){
-    const message = this.messageText || ''
+    let message = `Server running on port ${this.portNumber}` + (this.messageText ? '\n' + this.messageText : '')
+    if (this.name) message = this.name + ' ' +message.replace('S', 's')
     this.server?.listen(this.portNumber, this.host, undefined, function(){ console.log(message) })
     return this
   }
 }
 
-
-export function goatServer(){
-  return http.createServer(function(req,res){
-    const reqUrl = isRoute(req.url!)
-    if (reqUrl) {
-      console.log('is a route')
-      console.log(reqUrl)
-    }
-    res.end()
-  })
-}
-export function goatUp(server: http.Server<typeof http.IncomingMessage, typeof http.ServerResponse>, port: number, host: string, message: string): void {
-  server.listen(port, host, undefined, () => { console.log(message) })
-}
+// export function goatServer(){
+//   return http.createServer(function(req,res){
+//     const reqUrl = isRoute(req.url!)
+//     if (reqUrl) {
+//       console.log('is a route')
+//       console.log(reqUrl)
+//     }
+//     res.end()
+//   })
+// }
+// export function goatUp(server: http.Server<typeof http.IncomingMessage, typeof http.ServerResponse>, port: number, host: string, message: string): void {
+//   server.listen(port, host, undefined, () => { console.log(message) })
+// }
